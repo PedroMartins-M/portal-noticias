0d8d4f11dd523bb735b2a3d0a5d09bd48e778876
Pedro Martins <pedromartinsss996@gmail.com>
2026-06-02 21:39:07 -0300

    first commit

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,11 @@
+@extends("layouts.site")
+
+    @section("conteudo")
+
+    <div>
+        <h2 class="text-2xl font-bold">Contato</h2>
+        <p class="text-sm  text-slate-600">Estamos á disposição para ouvir você. se tiver dúvidas, sugestões, críticas,
+             informações para compartilhar ou desejar entrar em contato com nossa equipe editorial, utilize o formulário abaixo. abaixo. </p>
+    </div>
+
+    @endsection
